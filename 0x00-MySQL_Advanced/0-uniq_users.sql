985722a62a872697b3b9cdea4fe18d6ae2836b21
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-18 17:07:57 +0300

    Create users table with id, email, and name columns, enforcing uniqueness and non-null constraints

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- This script creates a table named 'users' if it does not already exist.
+-- The 'users' table has the following columns:
+-- - id: An integer that auto-increments and serves as the primary key.
+-- - email: A VARCHAR field with a maximum length of 255 characters, which must be unique and cannot be null.
+-- - name: A VARCHAR field with a maximum length of 255 characters, which cannot be null.
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL
+);
+
