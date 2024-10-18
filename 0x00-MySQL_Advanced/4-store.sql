dad249db1730f604fc28947febca6a8b54300420
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-18 21:27:12 +0300

    Create trigger to automatically decrease item quantity after new order insertion

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,30 +1,7 @@
-/*
+-- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+-- Context: Updating multiple tables for one action from your application can generate issue: network disconnection,
+-- crash, etc… to keep your data in a good shape, let MySQL do it for you!
 
-    Description:
-    This script creates a trigger named 'decrease_quantity_after_order' that is executed 
-    after a new row is inserted into the 'orders' table. The trigger decreases the 
-    quantity of the corresponding item in the 'items' table by the quantity specified 
-    in the new order.
-
-    Trigger Details:
-    - Trigger Name: decrease_quantity_after_order
-    - Event: AFTER INSERT on 'orders' table
-    - Action: Updates the 'quantity' field in the 'items' table by subtracting the 
-      quantity of the newly inserted order from the current quantity of the item.
-
-    Usage:
-    This trigger ensures that the inventory in the 'items' table is automatically 
-    updated to reflect the quantity of items ordered whenever a new order is placed.
-*/
-DELIMITER $$
-
-CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-END$$
-
-DELIMITER ;
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
