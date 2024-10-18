4b63b9521301bf05d3db96c4b21ce304e7efda4e
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-18 17:40:45 +0300

    Create users table with id, email, name, and country columns, enforcing unique and non-null constraints

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- This script creates a table named 'users' if it does not already exist.
+-- The 'users' table has the following columns:
+-- 1. id: An integer that auto-increments and serves as the primary key.
+-- 2. email: A varchar field with a maximum length of 255 characters, which must be unique and not null.
+-- 3. name: A varchar field with a maximum length of 255 characters, which can be null.
+-- 4. country: An enum field that can only take one of the following values: 'US', 'CA', 'MX', and cannot be null.
+
+    CREATE Table if NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country enum('US', 'CA', 'MX') NOT NULL
+);
