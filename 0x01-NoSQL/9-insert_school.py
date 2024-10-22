5440b70585c696a51c2b38d1f9c9d224888a58ee
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-23 02:04:21 +0300

    feat: add function to insert a document into MongoDB collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Insert a new document in a collection based on kwargs
+Prototype: def insert_school(mongo_collection, **kwargs):
+mongo_collection will be pymongo collection object
+Returns new _id
+"""
+
 def insert_school(mongo_collection, **kwargs):
     """
     Insert a document in a MongoDB collection.
