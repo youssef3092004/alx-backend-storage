2974a12c0f780b6f8964ca1a9c5f7f8dd4014b9e
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-10 23:28:21 +0200

    feat: Add replay function to display method call history
    - Implemented `replay` function to display the input-output history of a decorated function.
    - Retrieves method call inputs and outputs from Redis lists.
    - Prints the number of calls along with their inputs and corresponding outputs for easy tracking and debugging.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -46,3 +46,24 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(outp_m, str(fin))
         return fin
     return wrapper
+
+
+def replay(func: Callable):
+    """
+    Prototype: def replay(func: Callable):
+    Displays history of calls of a particular function
+    """
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m, k.decode('utf-8'), v.decode('utf-8'))
+        print(fin)
