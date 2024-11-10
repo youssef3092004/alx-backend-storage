9f5284f2e6dd66142c38b1bc0050ac44c7d29d37
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-10 23:27:31 +0200

    feat: Add call_history decorator to track method input and output
    - Implemented `call_history` decorator to store method call inputs and outputs in Redis.
    - Inputs are stored as strings in a list with the key `<method_name>:inputs`.
    - Outputs are stored as strings in a list with the key `<method_name>:outputs`.
    - Provides easy tracking and logging of method calls for debugging or analysis.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,24 @@ def count_calls(method: Callable) -> Callable:
         self._redis.incr(key_m)
         return method(self, *args, **kwds)
     return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """
+    Prototype: def call_history(method: Callable) -> Callable:
+    Returns a Callable
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        Prototype: def wrapper(self, *args, **kwds):
+        Returns wrapper
+        """
+        key_m = method.__qualname__
+        inp_m = key_m + ':inputs'
+        outp_m = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp_m, data)
+        fin = method(self, *args, **kwds)
+        self._redis.rpush(outp_m, str(fin))
+        return fin
+    return wrapper
